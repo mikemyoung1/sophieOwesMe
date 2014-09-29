2284c43c3befceade6df01af387c557b9b29b33b
Mike Young <mikemyoung@gmail.com>
2014-09-29 17:31:11 -0400

    first/last commit

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('sophieYoungPortfolioApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'ui.bootstrap'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/main',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('sophieYoungPortfolioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.myInterval = 3000;
+    $scope.slides = [
+    {image:'../images/NEW-PORTFOLIO-7.jpg'},
+    {image:'../images/NEW-PORTFOLIO-710.jpg' },
+    {image:'../images/NEW-PORTFOLIO-711.jpg' },
+    {image:'../images/NEW-PORTFOLIO-712.jpg' },
+    {image:'../images/NEW-PORTFOLIO-713.jpg' },
+    {image:'../images/NEW-PORTFOLIO-714.jpg' },
+    {image:'../images/NEW-PORTFOLIO-715.jpg' },
+    {image:'../images/NEW-PORTFOLIO-716.jpg' },
+    {image:'../images/NEW-PORTFOLIO-717.jpg', link: 'https://www.youtube.com/watch?v=VMuXQTKxgVc&feature=youtu.be', text: 'View Here'},
+    {image:'../images/NEW-PORTFOLIO-72.jpg' },
+    {image:'../images/NEW-PORTFOLIO-73.jpg' },
+    {image:'../images/NEW-PORTFOLIO-74.jpg' },
+    {image:'../images/NEW-PORTFOLIO-75.jpg' },
+    {image:'../images/NEW-PORTFOLIO-76.jpg' },
+    {image:'../images/NEW-PORTFOLIO-77.jpg', link: 'http://www.youtube.com/watch?v=tQbmc2jMXDM', text: 'View Here'},
+    {image:'../images/NEW-PORTFOLIO-78.jpg' },
+    {image:'../images/NEW-PORTFOLIO-79.jpg' }
+    ];
+  });
