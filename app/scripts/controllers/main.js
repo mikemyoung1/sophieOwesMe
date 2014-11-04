dc3165a3ef241354450984b92360801775233390
Mike Young <mikemyoung@gmail.com>
2014-11-04 10:24:38 -0500

    final version of sophies site... for now

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,24 +4,30 @@ angular.module('sophieYoungPortfolioApp')
   .controller('MainCtrl', function ($scope) {
     $scope.myInterval = 3000;
     $scope.slides = [
-    {image:'../images/NEW-PORTFOLIO-6.jpg', link: 'http://vimeo.com/109289980', text: 'View Here'},
-    {image:'../images/NEW-PORTFOLIO-7.jpg'},
-    {image:'../images/NEW-PORTFOLIO-72.jpg' },
-    {image:'../images/NEW-PORTFOLIO-73.jpg' },
-    {image:'../images/NEW-PORTFOLIO-74.jpg' },
-    {image:'../images/NEW-PORTFOLIO-75.jpg' },
-    {image:'../images/NEW-PORTFOLIO-76.jpg' },
-    {image:'../images/NEW-PORTFOLIO-78.jpg' },
-    {image:'../images/NEW-PORTFOLIO-79.jpg' },
-    {image:'../images/NEW-PORTFOLIO-710.jpg' },
-    {image:'../images/NEW-PORTFOLIO-711.jpg' },
-    {image:'../images/NEW-PORTFOLIO-712.jpg' },
-    {image:'../images/NEW-PORTFOLIO-713.jpg' },
-    {image:'../images/NEW-PORTFOLIO-714.jpg' },
-    {image:'../images/NEW-PORTFOLIO-715.jpg' },
-    {image:'../images/NEW-PORTFOLIO-716.jpg' },
-    {image:'../images/NEW-PORTFOLIO-718.jpg', link: 'https://www.youtube.com/watch?v=VMuXQTKxgVc&feature=youtu.be', text: 'View Here'},
-    {image:'../images/NEW-PORTFOLIO-77.jpg', link: 'http://www.youtube.com/watch?v=tQbmc2jMXDM', text: 'View Here'}
+    {image:'../images/NEW-PORTFOLIO-1.jpg'},
+    {image:'../images/NEW-PORTFOLIO-2.jpg' },
+    {image:'../images/NEW-PORTFOLIO-3.jpg' },
+    {image:'../images/NEW-PORTFOLIO-4.jpg' },
+    {image:'../images/NEW-PORTFOLIO-5.jpg' },
+    {image:'../images/NEW-PORTFOLIO-6.jpg' },
+    {image:'../images/NEW-PORTFOLIO-7.jpg', link: 'http://vimeo.com/109289980', text: 'View Here'},
+    {image:'../images/NEW-PORTFOLIO-8.jpg', link: 'https://www.youtube.com/watch?v=VMuXQTKxgVc&feature=youtu.be', text: 'View Here'},
+    {image:'../images/NEW-PORTFOLIO-9.jpg', link: 'http://www.youtube.com/watch?v=tQbmc2jMXDM', text: 'View Here'},
+    {image:'../images/NEW-PORTFOLIO-10.jpg' },
+    {image:'../images/NEW-PORTFOLIO-11.jpg' },
+    {image:'../images/NEW-PORTFOLIO-12.jpg' },
+    {image:'../images/NEW-PORTFOLIO-13.jpg' },
+    {image:'../images/NEW-PORTFOLIO-14.jpg' },
+    {image:'../images/NEW-PORTFOLIO-15.jpg' },
+    {image:'../images/NEW-PORTFOLIO-16.jpg' },
+    {image:'../images/NEW-PORTFOLIO-17.jpg' },
+    {image:'../images/NEW-PORTFOLIO-18.jpg' }
     ];
 
+    $('#playButton').click(function () {
+      $('#homeCarousel').carousel('cycle');
+    });
+    $('#pauseButton').click(function () {
+      $('#homeCarousel').carousel('pause');
+    });
   });
